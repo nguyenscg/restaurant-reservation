a15075354615c2a292f3266c703c8391ab30ebdc
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-07 06:01:52 -0600

    created form

--- front-end/src/reservations/CreateReservation.js
+++ front-end/src/reservations/CreateReservation.js
@@ -5,8 +5,6 @@ import { useHistory } from "react-router-dom"; // import useHistory hook
 
 function CreateReservation() {
     // inputs: first_name, last_name, mobile_number, reservation_date, reservation_time, people
-    // display a submit button that when clicked, saves new reservation, then displays the /dashboard page for the date of the new reservation
-    // display a cancel button that, when the button is clicked, returns the user to the previous page
     // display any error messages returned from API
     const initialState = { // initialState with all empty fields
         first_name: "",
@@ -18,6 +16,29 @@ function CreateReservation() {
     }
     const [reservation, setReservation] = useState(null); // initalize state of reservation
     const history = useHistory();
+
+    // display a submit button that when clicked, saves new reservation, then displays the /dashboard page for the date of the new reservation
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    // display a cancel button, when button is clicked, returns user to previous page
+    const handleCancel = (event) => {
+        history.push("/");
+    }
+
+    return (
+        <div>
+            <h1>Create Reservation:</h1>
+            <form>
+                <div className="form-group">
+                    <label htmlFor="first_name">First name</label>
+                    <input name="first_name" type="text"></input>
+                </div>
+            </form>
+            <button type="btn">Submit</button>
+        </div>
+    )
 }
 
 export default CreateReservation;
