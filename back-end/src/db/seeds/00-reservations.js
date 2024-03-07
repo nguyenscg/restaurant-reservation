0a1145ceb46412d832fccb62dc02941ae40dbe28
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-07 05:36:37 -0600

    imported reservations.json into seeds function to properly create reservations table

--- back-end/src/db/seeds/00-reservations.js
+++ back-end/src/db/seeds/00-reservations.js
@@ -1,3 +1,7 @@
+const reservations = require("./00-reservations.json"); // import the reservations data to use into seed
+
 exports.seed = function (knex) {
-  return knex.raw("TRUNCATE TABLE reservations RESTART IDENTITY CASCADE");
+  return knex
+  .raw("TRUNCATE TABLE reservations RESTART IDENTITY CASCADE")
+  .then(() => knex("reservations").insert(reservations));
 };
