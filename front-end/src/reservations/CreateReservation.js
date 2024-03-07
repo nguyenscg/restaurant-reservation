19e56799d498e18714861450a42ad4ea5ce4c1b4
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-07 05:53:42 -0600

    initialized initialState with empty fields and state of reservation

--- front-end/src/reservations/CreateReservation.js
+++ front-end/src/reservations/CreateReservation.js
@@ -1,12 +1,23 @@
 // path should be: /reservations/new
 
-import React from "react"; // import react
+import React, { useState } from "react"; // import React and useState hook
+import { useHistory } from "react-router-dom"; // import useHistory hook
 
 function CreateReservation() {
     // inputs: first_name, last_name, mobile_number, reservation_date, reservation_time, people
     // display a submit button that when clicked, saves new reservation, then displays the /dashboard page for the date of the new reservation
     // display a cancel button that, when the button is clicked, returns the user to the previous page
     // display any error messages returned from API
+    const initialState = { // initialState with all empty fields
+        first_name: "",
+        last_name: "",
+        mobile_number: "",
+        reservation_date: "",
+        reservation_time: "",
+        people: 0,
+    }
+    const [reservation, setReservation] = useState(null); // initalize state of reservation
+    const history = useHistory();
 }
 
 export default CreateReservation;
