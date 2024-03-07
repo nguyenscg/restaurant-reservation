f9edfceb747caf8f140c95a47527d8ae4387e7ec
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-07 05:46:26 -0600

    added pseudocode to createReservation component

--- front-end/src/reservations/CreateReservation.js
+++ front-end/src/reservations/CreateReservation.js
@@ -3,7 +3,10 @@
 import React from "react"; // import react
 
 function CreateReservation() {
-
+    // inputs: first_name, last_name, mobile_number, reservation_date, reservation_time, people
+    // display a submit button that when clicked, saves new reservation, then displays the /dashboard page for the date of the new reservation
+    // display a cancel button that, when the button is clicked, returns the user to the previous page
+    // display any error messages returned from API
 }
 
 export default CreateReservation;
