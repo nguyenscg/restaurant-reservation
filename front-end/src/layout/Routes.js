8c498c70f9a6a5f6ab5f0c977dc934f32c95ccb3
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-09 05:18:21 -0600

    imported create and edit reservation components and routed

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -4,6 +4,8 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
 import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
+import ReservationCreate from "../reservations/ReservationCreate";
+import ReservationEdit from "../reservations/ReservationEdit";
 
 /**
  * Defines all the routes for the application.
@@ -23,6 +25,12 @@ function Routes() {
       </Route>
       <Route path="/dashboard">
         <Dashboard date={today()} />
+      </Route>
+      <Route path="/reservations/new">
+        <ReservationCreate />
+      </Route>
+      <Route path="/reservations/:reservation_id/edit">
+        <ReservationEdit />
       </Route>
       <Route>
         <NotFound />
