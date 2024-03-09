e424930e51348de8a6a3befb4b946e0978cd8bd3
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-09 04:35:55 -0600

    created delete table function

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -123,3 +123,9 @@ export async function updateTable(table_id, reservation_id, signal) {
   };
   return await fetchJson(url, options);
 }
+
+export async function deleteTable(table_id, signal) {
+  const url = `${API_BASE_URL}/tables/${table_id}/seat`;
+  const options = { method: "DELETE", signal };
+  return await fetchJson(url, options);
+}
