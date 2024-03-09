136195054ffda228991d9bfb0f65c7ffbf0f48de
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-09 14:18:40 -0600

    created read, create, and update function to take the data that http makes and response with

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -7,7 +7,29 @@ async function list(req, res) {
   res.json({ data });
 }
 
+async function read(req, res) {
+  const data = res.locals.reservation;
+  res.json({ data });
+}
+
+async function create(req, res) {
+  const data = await service.create(req.body.data);
+  res.status(201).json({ data });
+}
+
+async function update(req, res) {
+  const updatedReservation = {
+    ...req.body.data,
+    reservation_id: res.locals.reservation.reservation_id,
+  };
+  const data = await service.update(updatedReservation);
+  res.status(200).json({ data });
+}
+
 
 module.exports = {
   list: asyncErrorBoundary(list),
+  read: asyncErrorBoundary(read),
+  create: asyncErrorBoundary(create),
+  update: asyncErrorBoundary(update),
 };
