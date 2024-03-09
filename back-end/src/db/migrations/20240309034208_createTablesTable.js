c155328a6e2d06ba103467ca5f9f7b535c21f250
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-09 03:53:26 -0600

    created tables table with inputs and data types

--- back-end/src/db/migrations/20240309034208_createTablesTable.js
+++ back-end/src/db/migrations/20240309034208_createTablesTable.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable("tables", (table) => {
+        table.increments("table_id").primary();
+        table.string("table_name").notNullable();
+        table.integer("capacity").notNullable();
+        table.integer("reservation_id");
+        table
+            .foreign("reservation_id")
+            .references("reservation_id")
+            .inTable("reservations")
+
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable("tables");  
 };
