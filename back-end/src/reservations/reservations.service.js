b087607b7d6f2bc658ec544c89c6483feed2a89b
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-09 12:13:56 -0600

    updated reservations service functions to CRUD method

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,9 +1,28 @@
 const knex = require("../db/connection");
 
+function list() {
+    return knex("reservations")
+        .select("*")
+        .orderBy("reservation_time");
+}
+
+
 function read(reservation_id) {
-    return knex("reservations").select("*").where({ reservation_id }).first();
+    return knex("reservations")
+        .select("*")
+        .where({ reservation_id })
+        .first();
+}
+
+function create(reservation) {
+    return knex("reservations")
+        .insert(reservation)
+        .returning("*")
+        .then((createdRecords) => createdRecords[0]);
 }
 
 module.exports = {
+    list,
     read,
+    create,
 }
