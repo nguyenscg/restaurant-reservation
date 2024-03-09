acf30741efc267265eab10ebdff5f57b24105544
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-09 11:50:57 -0600

    initial tablecreate component file setup

---
+++ front-end/src/tables/TablesCreate.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function TableCreate() {
+    const initialFormData = {
+        table_name: "",
+        capacity: 0,
+    };
+
+    const [formData, setFormData] = useState({...initialFormData});
+    
+}
+
+export default TableCreate;
