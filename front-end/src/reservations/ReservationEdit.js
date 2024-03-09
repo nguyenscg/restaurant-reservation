fa213569babf11450a6f1dabc160c0542ba0a148
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-09 03:27:50 -0600

    imported reservation form for edit reservation to use

--- front-end/src/reservations/ReservationEdit.js
+++ front-end/src/reservations/ReservationEdit.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
+import ReservationForm from "./ReservationForm";
 
 function ReservationEdit() {
 
     const [reservation, setReservation] = useState({});
+    const history = useHistory();
 
     const initialFormData = {
         first_name: "",
