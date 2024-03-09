dd2212eb892a24711463f1cd920e97ddbd6bcc4b
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-09 12:17:06 -0600

    added search function to service file to search by mobile_number

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -21,8 +21,25 @@ function create(reservation) {
         .then((createdRecords) => createdRecords[0]);
 }
 
+function update(updatedReservation) {
+    return knex("reservations")
+        .select("*")
+        .where({ reservation_id: updatedReservation.reservation_id })
+        .update(updatedReservation, "*");
+}
+
+function search(mobile_number) {
+    return knex("reservations")
+        .whereRaw("translate(mobile_number, '() -', '') like ?",
+        `%${mobile_number.replace(/\D/g, "")}%`
+        )
+        .orderBy("reservation_date");
+}
+
 module.exports = {
     list,
     read,
     create,
+    update,
+    search,
 }
