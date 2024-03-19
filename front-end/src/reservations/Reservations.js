908b4c6910428b41c0fe4bc27a780db147b21002
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-18 23:56:31 -0500

    added bootstrap styling to reservation list and reorganized code

--- front-end/src/reservations/Reservations.js
+++ front-end/src/reservations/Reservations.js
@@ -22,32 +22,30 @@ function Reservations() {
     }, []);
 
     return (
-    <table className="reservation">
-        <thead>
-            <tr>
-                <th scope="col">First Name</th>
-                <th scope="col">Last Name</th>
-                <th scope="col">Mobile Number</th>
-                <th scope="col">Reservation Date</th>
-                <th scope="col">Reservation Time</th>
-                <th scope="col">People</th>
+        <table className="table table-striped">
+            <thead className="thead-dark">
+                <tr>
+                    <th scope="col">First Name</th>
+                    <th scope="col">Last Name</th>
+                    <th scope="col">Mobile Number</th>
+                    <th scope="col">Reservation Date</th>
+                    <th scope="col">Reservation Time</th>
+                    <th scope="col">People</th>
                 </tr>
-                </thead>
-                <tbody>
-                    {reservations.map((reservation) => {
-                        return (
-                        <tr key={reservation.reservation_id}>
-                            <td>{reservation.first_name}</td>
-                            <td>{reservation.last_name}</td>
-                            <td>{reservation.mobile_number}</td>
-                            <td>{reservation.reservation_date}</td>
-                            <td>{reservation.reservation_time}</td>
-                            <td>{reservation.people}</td>
-                            </tr>
-                            );
-                            })}
-                            </tbody>
-                            </table>
+            </thead>
+            <tbody>
+                {reservations.map((reservation) => (
+                    <tr key={reservation.reservation_id}>
+                        <td>{reservation.first_name}</td>
+                        <td>{reservation.last_name}</td>
+                        <td>{reservation.mobile_number}</td>
+                        <td>{reservation.reservation_date}</td>
+                        <td>{reservation.reservation_time}</td>
+                        <td>{reservation.people}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
     );
 }
 
